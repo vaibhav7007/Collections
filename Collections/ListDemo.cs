0c9387aed5dc59f1a590b11b3a276978bff9cd82
vaibhav <vaibhavmehta524@gmail.com>
2023-03-13 05:15:55 +0530

    Modified to Demo List Collection Program

---
+++ Collections/ListDemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class ListDemo
+    {
+        public static void List()
+        {
+            List<int> list = new List<int>();
+            list.Add(13);
+            list.Add(30);
+            list.Add(10);
+            list.Add(25);
+            list.Add(63);
+            list.Add(78);
+            Console.WriteLine("The no of element present in list: " + list.Count);
+            foreach (int elem in list)
+            {
+                Console.WriteLine(elem);
+            }
+            //list.Clear();
+            Console.WriteLine("The no of element present in list: " + list.Count);
+            Console.WriteLine(list.Contains(30));
+            Console.WriteLine(list.Remove(10));
+            list.Reverse();
+            list.Sort();
+            list.RemoveAt(2);
+            foreach (int elem in list)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Collection programs");
+            Console.WriteLine("Please enter an option");
+            Console.WriteLine("1:List Demo");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    ListDemo.List();
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
+            }
+        }
+    }
+}
