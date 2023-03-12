8ef1afaa762cf06f85ac72bac5bd98e693579f73
vaibhav <vaibhavmehta524@gmail.com>
2023-03-13 05:20:18 +0530

    Modified to Demo Dictionary Collection Program

---
+++ Collections/DictionaryDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class DictionaryDemo
+    {
+        public static void Dictionary()
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            dict.Add(1, "One");
+            dict.Add(2, "Two");
+            dict.Add(3, "Three");
+            dict.Add(4, "Four");
+            foreach (KeyValuePair<int, string> element in dict)
+            {
+                Console.WriteLine("Key: " + element.Key + " " + "Value: " + element.Value);
+            }
+            Console.WriteLine("Elements present in Dictionary: " + dict.Count);
+            dict.Remove(1);
+            Console.WriteLine("Elements present in Dictionary: " + dict.Count);
+            foreach (KeyValuePair<int, string> element in dict)
+            {
+                Console.WriteLine("Key: " + element.Key + " " + "Value: " + element.Value);
+            }
+        }
+    }
+}
--- Collections/Program.cs
+++ Collections/Program.cs
@@ -8,12 +8,15 @@ namespace Collections
         {
             Console.WriteLine("Welcome to Collection programs");
             Console.WriteLine("Please enter an option");
-            Console.WriteLine("1:List Demo");
+            Console.WriteLine("1:List Demo\n2:Dictionary Demo");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     ListDemo.List();
+                    break;
+                case 2:
+                    DictionaryDemo.Dictionary();
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
