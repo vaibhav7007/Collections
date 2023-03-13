2fc70d9ccca56b8701aa430661e5800039dfced1
vaibhav <vaibhavmehta524@gmail.com>
2023-03-13 05:31:00 +0530

    Modified to Demo Stack Collection Program

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -8,7 +8,7 @@ namespace Collections
         {
             Console.WriteLine("Welcome to Collection programs");
             Console.WriteLine("Please enter an option");
-            Console.WriteLine("1:List Demo\n2:Dictionary Demo\n3:SortedList Demo");
+            Console.WriteLine("1:List Demo\n2:Dictionary Demo\n3:SortedList Demo\n4:Stack Demo");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -20,6 +20,9 @@ namespace Collections
                     break;
                 case 3:
                     SortedListDemo.SortedList();
+                    break;
+                case 4:
+                    StackDemo.Stack();
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
---
+++ Collections/StackDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class StackDemo
+    {
+        public static void Stack()
+        {
+            Stack stack = new Stack();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(40);
+            Console.WriteLine("The count of element is: " + stack.Count);
+            foreach (int ele in stack)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.WriteLine("The top most element in stack: " + stack.Peek());
+            Console.WriteLine("The element is remove in stack: " + stack.Pop());
+            foreach (int ele in stack)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
