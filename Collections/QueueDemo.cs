c13420b07d0de710240431635bd5cecdf3b5d423
vaibhav <vaibhavmehta524@gmail.com>
2023-03-13 05:36:28 +0530

    Modified to Demo Queue Collection Program

---
+++ Collections/QueueDemo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class QueueDemo
+    {
+        public static void Queue()
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(12);
+            queue.Enqueue(24);
+            queue.Enqueue(36);
+            queue.Enqueue(72);
+            foreach (var ele in queue)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.WriteLine("The element present in queue: " + queue.Count);
+            queue.Dequeue();
+            Console.WriteLine(queue.Contains(30));
+            foreach (var ele in queue)
+            {
+                Console.WriteLine(ele);
+            }
+            queue.Peek();
+        }
+    }
+}
